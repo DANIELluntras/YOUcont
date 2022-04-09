8fd8cba733840499f48c0070aed05e3b9bb434c4
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-04-09 23:30:54 +0300

    new project you cont name

---
+++ prima.js
@@ -0,0 +1,10 @@
+console.log("tata");
+const btton = document.getElementById("buttond")
+
+function mouseOver() {
+    btton.style.background = "yellow";
+};
+
+function mouseOut() {
+    btton.style.background = "white";
+};
