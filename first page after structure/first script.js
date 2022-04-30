b237d580bde3f405fa152634d08fceba7d20786c
luntras Daniel <luntrasdaniel12@gmail.com>
2022-04-30 17:44:15 +0300

    learn style javascript

--- first page after structure/first script.js
+++ first page after structure/first script.js
@@ -1,1 +1,24 @@
-console.log('daniel')
+(function() {
+    const paragra = document.querySelector('h1');
+    paragra.style.color = 'black';
+    document.querySelector('body').addEventListener('click', function() {
+        paragra.style.color = 'green';
+
+    })
+})();
+
+(function() {
+    const buton = document.querySelector('button');
+    document.querySelector('button').addEventListener('click', function() {
+        buton.style.color = 'yellow'
+
+    })
+})();
+
+(function() {
+    const dnaiel = document.getElementById('haha');
+    dnaiel.style.color = 'red'
+    document.getElementById('haha').addEventListener('click', function() {
+        dnaiel.style.color = 'blue'
+    })
+})();
