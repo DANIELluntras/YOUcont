4796acc3181d925b35ccf472d0d54c80e721f289
luntras Daniel <luntrasdaniel12@gmail.com>
2022-05-10 17:01:44 +0300

    design

--- first page after structure/first script.js
+++ first page after structure/first script.js
@@ -1,6 +1,6 @@
 (function() {
     const paragra = document.querySelector('h1');
-    paragra.style.color = 'black';
+    paragra.style.color = 'blue';
     document.querySelector('body').addEventListener('click', function() {
         paragra.style.color = 'green';
 
@@ -20,5 +20,31 @@
     dnaiel.style.color = 'red'
     document.getElementById('haha').addEventListener('click', function() {
         dnaiel.style.color = 'blue'
+    })
+})();
+
+(function() {
+    const paragraf = document.querySelector('p');
+    paragraf.style.color = 'red';
+    paragraf.style.fontWeight = '400';
+    paragraf.style.textAlign = 'center';
+    paragraf.style.fontSize = '40px';
+
+
+    document.getElementById('haha').addEventListener('click', function() {
+        paragraf.style.color = 'blue'
+    })
+})();
+
+(function() {
+    const nav = document.querySelector('main');
+
+    nav.style.fontWeight = '400';
+    nav.style.textAlign = 'center';
+    nav.style.fontSize = '40px';
+
+
+    document.getElementById('haha').addEventListener('click', function() {
+        nav.style.color = 'blue'
     })
 })();
